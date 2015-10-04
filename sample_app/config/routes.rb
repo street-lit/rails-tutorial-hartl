3a68798c2c3affc80a48947c5082b5471f4f96e5
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-03 19:23:05 -0500

    Create partials and add named routes

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/help',     to: 'static_pages#help',    as: 'help'
+  get '/about',    to: 'static_pages#about',   as: 'about'
+  get '/contact',  to: 'static_pages#contact', as: 'contact'
+
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
 end
