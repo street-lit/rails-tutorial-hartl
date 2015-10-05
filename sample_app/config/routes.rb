a13b796c01204d030b3b8315ca5f9729a89f3053
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-04 23:43:27 -0500

    Create login view

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
-  get 'users/new'
-
-  get '/help',     to: 'static_pages#help',    as: 'help'
-  get '/about',    to: 'static_pages#about',   as: 'about'
-  get '/contact',  to: 'static_pages#contact', as: 'contact'
-  get '/signup',   to: 'users#new',            as: 'signup'
   resources :users
-  root 'static_pages#home'
+
+  get    '/help',     to: 'static_pages#help',    as: 'help'
+  get    '/about',    to: 'static_pages#about',   as: 'about'
+  get    '/contact',  to: 'static_pages#contact', as: 'contact'
+  get    '/signup',   to: 'users#new',            as: 'signup'
+  get    '/login',    to: 'sessions#new',         as: 'login'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy',     as: 'logout'
+
+  root   'static_pages#home'
 end
