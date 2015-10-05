d29e27e4f11bb88fbd1e1f2e79c6b62aab4fdbf1
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-04 21:36:50 -0500

    Finish user signup

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/about',    to: 'static_pages#about',   as: 'about'
   get '/contact',  to: 'static_pages#contact', as: 'contact'
   get '/signup',   to: 'users#new',            as: 'signup'
-
+  resources :users
   root 'static_pages#home'
 end
